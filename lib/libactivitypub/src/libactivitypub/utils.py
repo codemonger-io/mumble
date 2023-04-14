3a3d8ec101939ef6b3cabc1d7adec14be809b1bf
Kikuo Emoto <kemoto@codemonger.io>
2023-04-14 23:54:20 +0900

    feat(libactivitypub): add "acct" URI parser
    - Adds a new function `parse_acct_uri` that parses an "acct" URI to
      `utils` submodule. An "acct" URI is specified to the `resource` query
      parameter in a WebFinger request.

--- lib/libactivitypub/src/libactivitypub/utils.py
+++ lib/libactivitypub/src/libactivitypub/utils.py
@@ -24,3 +24,19 @@ def parse_webfinger_id(account: str) -> Tuple[str, str]:
             'WebFinger ID must be in the form "<name>@<domain-name>";'
             f' e.g., "gargron@mastodon.social": {account}',
         ) from exc
+
+
+def parse_acct_uri(uri: str) -> Tuple[str, str]:
+    """Parses a given "acct" URI; e.g., resource URI given to WebFinger.
+
+    ``uri`` must be in the form "acct:<name>@<domain-name>"; e.g.,
+    "acct:gargron@mastodon.social".
+
+    :returns: tuple of the account and domain name.
+
+    :raises ValueError: if ``uri`` is not a valid "acct" URI.
+    """
+    prefix = 'acct:'
+    if not uri.startswith(prefix):
+        raise ValueError(f'"acct" URI must starts with "{prefix}": {uri}')
+    return parse_webfinger_id(uri[len(prefix):])
--- lib/libactivitypub/tests/test_utils.py
+++ lib/libactivitypub/tests/test_utils.py
@@ -3,7 +3,7 @@
 """Tests ``libactivitypub.utils``.
 """
 
-from libactivitypub.utils import parse_webfinger_id
+from libactivitypub.utils import parse_acct_uri, parse_webfinger_id
 import pytest
 
 
@@ -20,3 +20,26 @@ def test_parse_webfinger_id_without_atmark():
     """
     with pytest.raises(ValueError):
         parse_webfinger_id('gargron')
+
+
+def test_parse_acct_uri_with_valid_uri():
+    """Tests ``parse_acct_uri`` with a valid URI
+    ("acct:gargron@mastodon.social").
+    """
+    assert parse_acct_uri('acct:gargron@mastodon.social') == ('gargron', 'mastodon.social')
+
+
+def test_parse_acct_uri_without_acct_scheme():
+    """Tests ``parse_acct_uri`` with a string without the "acct" scheme
+    ("gargron@mastodon.social").
+    """
+    with pytest.raises(ValueError):
+        parse_acct_uri('gargron@mastodon.social')
+
+
+def test_parse_acct_uri_without_atmark():
+    """Tests ``parse_acct_uri`` with a string without an atmark
+    ("acct:gargron").
+    """
+    with pytest.raises(ValueError):
+        parse_acct_uri('acct:gargron')
