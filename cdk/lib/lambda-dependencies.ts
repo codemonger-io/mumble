ffc22bc1c498c8210113d8b4a78f0abd8296158b
Kikuo Emoto <kemoto@codemonger.io>
2023-04-23 09:53:25 +0900

    feat(cdk): add third-party dependencies
    - `LambdaDependencies` provisions a new Lambda layer that bundles
      third-party dependencies of Lambda functions. It includes the
      following packages so far:
        - `pycryptodome`
        - `pytz`
        - `requests`

--- cdk/lib/lambda-dependencies.ts
+++ cdk/lib/lambda-dependencies.ts
@@ -1,6 +1,7 @@
 import * as path from 'path';
 import { aws_lambda as lambda } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { PythonLayerVersion } from '@aws-cdk/aws-lambda-python-alpha';
 
 import { PythonLibraryLayer } from 'cdk2-python-library-layer';
 
@@ -8,6 +9,8 @@ import { PythonLibraryLayer } from 'cdk2-python-library-layer';
  * CDK construct that provisions dependencies common among Lambda functions.
  */
 export class LambdaDependencies extends Construct {
+  /** Lambda layer of common third party packages. */
+  readonly libCommons: lambda.ILayerVersion;
   /** Lambda layer of `libactivitypub`. */
   readonly libActivityPub: lambda.ILayerVersion;
   /** Lambda layer of `libmumble`. */
@@ -16,6 +19,15 @@ export class LambdaDependencies extends Construct {
   constructor(scope: Construct, id: string) {
     super(scope, id);
 
+    this.libCommons = new PythonLayerVersion(this, 'LibCommons', {
+      description: 'Lambda layer for common third party packages',
+      compatibleRuntimes: [
+        lambda.Runtime.PYTHON_3_8,
+        lambda.Runtime.PYTHON_3_9,
+      ],
+      compatibleArchitectures: [lambda.Architecture.ARM_64],
+      entry: path.join('lambda', 'dependencies'),
+    });
     this.libActivityPub = new PythonLibraryLayer(this, 'LibActivityPub', {
       description: 'Lambda layer for libactivitypub',
       runtime: lambda.Runtime.PYTHON_3_8,
