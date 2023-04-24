99262eb132248a151b762510b01758e53a14dd94
Kikuo Emoto <kemoto@codemonger.io>
2023-04-24 18:59:37 +0900

    feat(cdk): add datetime formatter util
    - Adds the following functions to `libmumble.utils`:
        - `format_yyyymmdd_hhmmss_ssssss`
        - `current_yyyymmdd_hhmmss_ssssss`

--- cdk/lambda/libmumble/src/libmumble/utils.py
+++ cdk/lambda/libmumble/src/libmumble/utils.py
@@ -3,6 +3,9 @@
 """Provides miscellaneous utilities.
 """
 
+from datetime import datetime, timezone
+
+
 def to_urlsafe_base64(b64: str) -> str:
     """Converts a standard Base64-encoded string into a URL-safe string.
 
@@ -10,3 +13,26 @@ def to_urlsafe_base64(b64: str) -> str:
     Removes trailing '='.
     """
     return b64.rstrip('=').replace('+', '-').replace('/', '_')
+
+
+def format_yyyymmdd_hhmmss_ssssss(time: datetime) -> str:
+    """Formats a given datetime into "yyyy-mm-ddTHH:MM:ss.SSSSSSZ" form.
+
+    The timzone is adjusted into UTC.
+
+    :raises ValueError: if ``time`` is naive.
+    """
+    if time.tzinfo is None:
+        raise ValueError('datetime must be timezone-aware')
+    offset = time.tzinfo.utcoffset(time)
+    if offset is None:
+        raise ValueError('datetime must be timezone-aware')
+    if offset != 0:
+        time = time.astimezone(timezone.utc)
+    return time.strftime('%Y-%m-%dT%H:%M:%S.%fZ')
+
+
+def current_yyyymmdd_hhmmss_ssssss() -> str:
+    """Returns the current timestamp in the form: "yyyy-mm-ddTHH:MM:ss.SSSSSSZ".
+    """
+    return format_yyyymmdd_hhmmss_ssssss(datetime.now(tz=timezone.utc))
--- cdk/lambda/libmumble/tests/test_utils.py
+++ cdk/lambda/libmumble/tests/test_utils.py
@@ -3,7 +3,10 @@
 """Tests ``libmumble.utils``.
 """
 
-from libmumble.utils import to_urlsafe_base64
+from datetime import datetime, timezone
+from libmumble.utils import format_yyyymmdd_hhmmss_ssssss, to_urlsafe_base64
+import pytest
+import pytz
 
 
 def test_to_urlsafe_base64():
@@ -26,3 +29,26 @@ def test_to_urlsafe_base64_with_empty_string():
     """Tests ``to_urlsafe_base64`` with an empty string.
     """
     assert to_urlsafe_base64('') == ''
+
+
+def test_format_yyyymmdd_hhmmss_ssssss():
+    """Tests ``format_yyyymmdd_hhmmss_ssssss``.
+    """
+    time = datetime(2023, 4, 24, 17, 2, 23, 123456, tzinfo=timezone.utc)
+    assert format_yyyymmdd_hhmmss_ssssss(time) == '2023-04-24T17:02:23.123456Z'
+
+
+def test_format_yyyymmdd_hhmmss_ssssss_with_jst():
+    """Tests ``format_yyyymmdd_hhmmss_ssssss`` with a datetime in JST.
+    """
+    jst = pytz.timezone('Asia/Tokyo')
+    time = jst.localize(datetime(2023, 4, 24, 23, 50, 9, 789))
+    assert format_yyyymmdd_hhmmss_ssssss(time) == '2023-04-24T14:50:09.000789Z'
+
+
+def test_format_yyyymmdd_hhmmss_ssssss_with_naive():
+    """Tests ``format_yyyymmdd_hhmmss_ssssss`` with a naive datetime.
+    """
+    time = datetime(2023, 4, 24, 23, 57, 59, 2099)
+    with pytest.raises(ValueError):
+        format_yyyymmdd_hhmmss_ssssss(time)
