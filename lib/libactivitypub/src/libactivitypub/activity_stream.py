0fe29a2d3e9cc1b6bf6eb8fb0a5ba2a025e2f1ed
Kikuo Emoto <kemoto@codemonger.io>
2023-04-26 13:48:46 +0900

    feat(libactivitypub): add post for ActivityStreams
    - Introduces a new function `post` to `activity_stream` submodule, which
      makes a POST request signed with a given private key. It is not
      actually required by ActivityStreams but Mastodon.
    
      Adds the JSON-LD context for ActivityStreams as
      `ACTIVITY_STREAMS_CONTEXT`.

--- lib/libactivitypub/src/libactivitypub/activity_stream.py
+++ lib/libactivitypub/src/libactivitypub/activity_stream.py
@@ -4,24 +4,40 @@
 ActivityStream.
 """
 
+from email.utils import formatdate
 import logging
-from typing import Any, Dict
+from typing import Any, Dict, TypedDict
+from urllib.parse import urlparse
 import requests
 from .mime_types import DEFAULT_ACTIVITY_STREAM_MIME_TYPE
+from .signature import digest_request_body, make_signature_header
 
 
 LOGGER = logging.getLogger('libactivitypub.activity_stream')
 LOGGER.setLevel(logging.DEBUG)
 
+ACTIVITY_STREAMS_CONTEXT = 'https://www.w3.org/ns/activitystreams'
+"""JSON-LD context for ActivityStream."""
 
 DEFAULT_REQUEST_TIMEOUT = 30.0
 """Default timeout of requests."""
 
 
+class PrivateKey(TypedDict):
+    """Private key.
+    """
+    key_id: str
+    """Key ID associated with the private key."""
+    private_key_pem: str
+    """PEM representation of the private key."""
+
+
 def get(endpoint: str) -> Dict[str, Any]:
     """Makes a GET request to a given endpoint.
 
-    :raises requests.HTTPError: if an HTTP request fails.
+    :raises requests.HTTPError: if the HTTP request fails.
+
+    :raises requests.Timeout: if the request times out.
     """
     res = requests.get(
         endpoint,
@@ -32,3 +48,61 @@ def get(endpoint: str) -> Dict[str, Any]:
     )
     res.raise_for_status()
     return res.json()
+
+
+def post(
+    endpoint: str,
+    body: bytes,
+    private_key: PrivateKey,
+) -> requests.Response:
+    """Makes a POST request to a given endpoint.
+
+    The request is signed with ``private_key``.
+
+    The signature header will include the following header values:
+    * (request-target): extracted from ``endpoint``
+    * host: extracted from ``endpoint``
+    * date: use current datetime
+    * digest: calculated from ``body``
+    * content-type: "application/json"
+
+    :raises ValueError: if ``endpoint`` contains no host,
+    or if the private key is invalid.
+
+    :raises requests.HTTPError: if the HTTP request fails.
+
+    :raises requests.Timeout: if the request times out.
+    """
+    parsed_uri = urlparse(endpoint)
+    path = parsed_uri.path
+    host = parsed_uri.hostname
+    if not host:
+        raise ValueError(f'no host in endpoint: {endpoint}')
+    date = formatdate(usegmt=True)
+    body_digest = digest_request_body(body)
+    signature_header = make_signature_header(
+        private_key['key_id'],
+        private_key['private_key_pem'],
+        [
+            ('(request-target)', f'post {path}'),
+            ('host', host),
+            ('date', date),
+            ('digest', body_digest),
+            ('content-type', 'application/json'),
+        ],
+    )
+    LOGGER.debug('signature header: %s', signature_header)
+    res = requests.post(
+        endpoint,
+        data=body,
+        headers={
+            'Accept': DEFAULT_ACTIVITY_STREAM_MIME_TYPE,
+            'Content-Type': 'application/json',
+            'Date': date,
+            'Digest': body_digest,
+            'Signature': signature_header,
+        },
+        timeout=DEFAULT_REQUEST_TIMEOUT,
+    )
+    res.raise_for_status()
+    return res
