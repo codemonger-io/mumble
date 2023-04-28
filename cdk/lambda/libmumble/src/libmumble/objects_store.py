9cfe709a1685dcf2780e4693286a78c863dc7821
Kikuo Emoto <kemoto@codemonger.io>
2023-04-29 03:27:21 +0900

    feat(cdk): add ObjectKey (libmumble)
    - `libmumble.objects_store` introduces a new class `ObjectKey` to
      represent an object in an S3 bucket. A new function
      `dict_as_object_key` casts a generic `dict` to `ObjectKey` while
      checking compatibility.
    
      Introduces `get_username_from_staging_outbox_key` that extracts the
      username from a given object key in the staging outbox folder in an
      S3 bucket. `get_username_from_key` implements the common part between
      `get_username_from_inbox_key` and
      `get_username_from_staging_outbox_key`.

--- cdk/lambda/libmumble/src/libmumble/objects_store.py
+++ cdk/lambda/libmumble/src/libmumble/objects_store.py
@@ -5,10 +5,46 @@
 
 import logging
 import re
+from typing import Any, Dict, TypedDict
 
 
 LOGGER = logging.getLogger('libmumble.objects_store')
 LOGGER.setLevel(logging.DEBUG)
+
+
+class ObjectKey(TypedDict):
+    """``dict`` representation of an object key in an S3 bucket.
+    """
+    bucket: str
+    """Name of the S3 bucket."""
+    key: str
+    """Key of the object."""
+
+
+def dict_as_object_key(d: Dict[str, Any]) -> ObjectKey: # pylint: disable=invalid-name
+    """Casts a given ``dict`` as an ``ObjectKey``.
+
+    :raises TypeError: if ``d`` is incompatible with ``ObjectKey``.
+    """
+    if not isinstance(d.get('bucket'), str):
+        raise TypeError(f'"bucket" must be str but ${type(d.get("bucket"))}')
+    if not isinstance(d.get('key'), str):
+        raise TypeError(f'"key" must be str but ${type(d.get("key"))}')
+    # unfortunately, above checks cannot convince d is ObjectKey
+    return d # type: ignore
+
+
+def get_username_from_key(prefix: str, key: str) -> str:
+    """Extracts the username from a given object key.
+
+    :param str prefix: prefix of the key. any characters reserved by regex
+    must be properyly escaped.
+    """
+    pattern = f'^{prefix}\\/users\\/([^/]+)\\/'
+    match = re.match(pattern, key)
+    if match is None:
+        raise ValueError(f'no username in object key: {key}')
+    return match.group(1)
 
 
 def get_username_from_inbox_key(key: str) -> str:
@@ -16,8 +52,12 @@ def get_username_from_inbox_key(key: str) -> str:
 
     :raises ValueError: if ``key`` is not in the inbox.
     """
-    pattern = r'^inbox\/users\/([^/]+)\/'
-    match = re.match(pattern, key)
-    if match is None:
-        raise ValueError(f'no username in object key: {key}')
-    return match.group(1)
+    return get_username_from_key('inbox', key)
+
+
+def get_username_from_staging_outbox_key(key: str) -> str:
+    """Extracts the username from a given object key in the staging outbox.
+
+    :raises ValueError: if ``key`` is not in the staging outbox.
+    """
+    return get_username_from_key('staging', key)
--- cdk/lambda/libmumble/tests/test_objects_store.py
+++ cdk/lambda/libmumble/tests/test_objects_store.py
@@ -3,8 +3,65 @@
 """Tests ``libmumble.objects_store``.
 """
 
+from typing import Any, Dict
 import pytest
-from libmumble.objects_store import get_username_from_inbox_key
+from libmumble.objects_store import (
+    dict_as_object_key,
+    get_username_from_inbox_key,
+    get_username_from_staging_outbox_key,
+)
+
+
+def test_dict_as_object_key_with_valid_dict():
+    """Tests ``dict_as_object_key`` with a valid ``dict``.
+    """
+    obj: Dict[str, Any] = {
+        'bucket': 'bucket-for-tests',
+        'key': 'inbox/users/kemoto/object.json',
+    }
+    assert dict_as_object_key(obj) == obj
+
+
+def test_dict_as_object_key_without_bucket():
+    """Tests ``dict_as_object_key`` without "bucket".
+    """
+    obj: Dict[str, Any] = {
+        'key': 'inbox/users/kemoto/object.json',
+    }
+    with pytest.raises(TypeError):
+        dict_as_object_key(obj) == obj
+
+
+def test_dict_as_object_key_with_non_str_bucket():
+    """Tests ``dict_as_object_key`` with non-str "bucket".
+    """
+    obj: Dict[str, Any] = {
+        'bucket': 123,
+        'key': 'inbox/users/kemoto/object.json',
+    }
+    with pytest.raises(TypeError):
+        dict_as_object_key(obj) == obj
+
+
+def test_dict_as_object_key_without_key():
+    """Tests ``dict_as_object_key`` without "key".
+    """
+    obj: Dict[str, Any] = {
+        'bucket': 'bucket-for-tests',
+    }
+    with pytest.raises(TypeError):
+        dict_as_object_key(obj) == obj
+
+
+def test_dict_as_object_key_with_non_str_key():
+    """Tests ``dict_as_object_key`` with non-str "key".
+    """
+    obj: Dict[str, Any] = {
+        'bucket': 'bucket-for-tests',
+        'key': 123,
+    }
+    with pytest.raises(TypeError):
+        dict_as_object_key(obj) == obj
 
 
 def test_get_username_from_inbox_key_with_valid_key():
@@ -20,3 +77,20 @@ def test_get_username_from_inbox_key_with_invalid_key():
     key = 'outbox/users/kemoto/activity.json'
     with pytest.raises(ValueError):
         get_username_from_inbox_key(key)
+
+
+def test_get_username_from_staging_outbox_key_with_valid_key():
+    """Tests ``get_username_from_staging_outbox_key`` with a valid staging
+    outbox object key.
+    """
+    key = 'staging/users/kemoto/note.json'
+    assert get_username_from_staging_outbox_key(key) == 'kemoto'
+
+
+def test_get_username_from_staging_outbox_key_with_invalid_key():
+    """Tests ``get_username_from_staging_outbox_key`` with an invalid staging
+    outbox object key.
+    """
+    key = 'inbox/users/kemoto/activity.json'
+    with pytest.raises(ValueError):
+        get_username_from_staging_outbox_key(key)
