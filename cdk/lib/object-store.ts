48b1e261195846c6464283962c20cede2b5e8f16
Kikuo Emoto <kemoto@codemonger.io>
2023-04-29 01:53:00 +0900

    feat(cdk): add staging outbox
    - `ObjectStore` introduces the staging outbox folder that stores objects
      staged for delivery. For instance, this folder stores a "Note" object
      posted by a user, which does not have `id` yet. `ObjectStore` provides
      a new EventBridge rule `stagingOutboxObjectCreatedRule` so that a
      state machine execution can start when an oject is staged.

--- cdk/lib/object-store.ts
+++ cdk/lib/object-store.ts
@@ -10,20 +10,73 @@ import { Construct } from 'constructs';
 
 /** Paht prefix of the inbox. */
 export const INBOX_PREFIX = 'inbox/';
+/**
+ * Path prefix of the staging outbox.
+ *
+ * @remarks
+ *
+ * The staging outbox is the folder where objects are pushed before sending
+ * through the outbox.
+ *
+ * The staging outbox provides an opportunity to arrange an object before
+ * sending it; e.g., assigning an ID, timestamping.
+ */
+export const STAGING_OUTBOX_PREFIX = 'staging/';
 
-/** CDK construct that provisions the S3 bucket for objects. */
+/**
+ * CDK construct that provisions the S3 bucket for objects.
+ *
+ * @remarks
+ *
+ * #### Inbox
+ *
+ * The inbox (`inbox`) folder stores activities received from other servers.
+ *
+ * Every user `{username}` has a dedicated folder in the inbox folder:
+ * `inbox/users/{username}`
+ *
+ * I am planning to allocate the shared inbox at `inbox/shared`.
+ *
+ * #### Staging outbox
+ *
+ * The staging outbox (`staging`) folder stores activities to be delivered to
+ * other servers.
+ *
+ * Every user `{username}` has a dedicated folder in the staging outbox folder:
+ * `staging/users/{username}`
+ *
+ * #### Objects
+ *
+ * The objects (`objects`) folder stores non-activity objects.
+ *
+ * Every user `{username}` has a dedicated folder in the objects folder:
+ * `objects/users/{username}`
+ *
+ * I am planning to further split folders in user's folder:
+ * - `objects/users/{username}/posts`: for posts
+ * - `objects/users/{username}/media`: for media files: e.g., images, videos
+ */
 export class ObjectStore extends Construct {
   /** S3 bucket for objects. */
   readonly objectsBucket: s3.IBucket;
   /**
-   * EventBridge Rule that triggers when a new object is created in the inbox
+   * EventBridge rule that triggers when a new object is created in the inbox
    * folder in the S3 bucekt.
    *
    * @remarks
    *
-   * Add a target to handle events.
+   * Please add a target to handle events.
    */
   readonly inboxObjectCreatedRule: events.Rule;
+  /**
+   * EventBridge Rule that triggers when a new object is created in the staging
+   * outbox folder in the S3 bucket.
+   *
+   * @remarks
+   *
+   * Please add a target to handle events.
+   */
+  readonly stagingOutboxObjectCreatedRule: events.Rule;
 
   constructor(scope: Construct, id: string) {
     super(scope, id);
@@ -35,7 +88,7 @@ export class ObjectStore extends Construct {
       removalPolicy: RemovalPolicy.RETAIN,
     });
 
-    // S3 notification events should be sent to the default event bus?
+    // S3 notification events should be sent to the default event bus
     const eventBus = events.EventBus.fromEventBusName(
       this,
       'DefaultEventBus',
@@ -65,6 +118,28 @@ export class ObjectStore extends Construct {
         },
       },
     );
+    // - triggers when a new object is created in the staging outbox
+    this.stagingOutboxObjectCreatedRule = new events.Rule(
+      this,
+      'StagingOutboxObjectCreatedRule',
+      {
+        description: 'Triggers when a new object is created in the staging outbox folder in the S3 bucket',
+        eventBus,
+        enabled: true,
+        eventPattern: {
+          account: [Stack.of(this).account],
+          region: [Stack.of(this).region],
+          source: ['aws.s3'],
+          resources: [this.objectsBucket.bucketArn],
+          detailType: ['Object Created'],
+          detail: {
+            object: {
+              key: [{ prefix: STAGING_OUTBOX_PREFIX }],
+            },
+          },
+        },
+      },
+    );
   }
 
   /** Grants a given principal "Put" access to the inbox. */
@@ -75,5 +150,10 @@ export class ObjectStore extends Construct {
   /** Grants a given principal "Get" access from the inbox. */
   grantGetFromInbox(grantee: iam.IGrantable): iam.Grant {
     return this.objectsBucket.grantRead(grantee, INBOX_PREFIX + '*');
+  }
+
+  /** Grants a given principal "Get" access from the staging outbox. */
+  grantGetFromStagingOutbox(grantee: iam.IGrantable): iam.Grant {
+    return this.objectsBucket.grantRead(grantee, STAGING_OUTBOX_PREFIX + '*');
   }
 }
