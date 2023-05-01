a6fd0c3f93c65f14bf8caa103a1d53a9061dfe9e
Kikuo Emoto <kemoto@codemonger.io>
2023-05-01 18:37:51 +0900

    feat(cdk): add outbox event rule
    - `ObjectStore` introduces a new EventBridge rule that triggers when an
      object is created in the outbox.
    
      Also introduces new permissions:
        - `grantGetFromOutbox`
        - `grantPutIntoStagingOutbox`
    
      Introduces a private method `objectCreatedEventPattern` to reduce
      repeated event pattern definition.

--- cdk/lib/object-store.ts
+++ cdk/lib/object-store.ts
@@ -73,7 +73,7 @@ export class ObjectStore extends Construct {
    */
   readonly inboxObjectCreatedRule: events.Rule;
   /**
-   * EventBridge Rule that triggers when a new object is created in the staging
+   * EventBridge rule that triggers when a new object is created in the staging
    * outbox folder in the S3 bucket.
    *
    * @remarks
@@ -81,6 +81,15 @@ export class ObjectStore extends Construct {
    * Please add a target to handle events.
    */
   readonly stagingOutboxObjectCreatedRule: events.Rule;
+  /**
+   * EventBridge rule that triggers when a new object is created in the outbox
+   * folder in the S3 bucket.
+   *
+   * @remarks
+   *
+   * Please add a target to handle events.
+   */
+  readonly outboxObjectCreatedRule: events.Rule;
 
   constructor(scope: Construct, id: string) {
     super(scope, id);
@@ -108,18 +117,7 @@ export class ObjectStore extends Construct {
         description: 'Triggers when a new object is created in the inbox folder in the S3 bucket',
         eventBus,
         enabled: true,
-        eventPattern: {
-          account: [Stack.of(this).account],
-          region: [Stack.of(this).region],
-          source: ['aws.s3'],
-          resources: [this.objectsBucket.bucketArn],
-          detailType: ['Object Created'],
-          detail: {
-            object: {
-              key: [{ prefix: INBOX_PREFIX }],
-            },
-          },
-        },
+        eventPattern: this.objectCreatedEventPattern(INBOX_PREFIX),
       },
     );
     // - triggers when a new object is created in the staging outbox
@@ -130,18 +128,18 @@ export class ObjectStore extends Construct {
         description: 'Triggers when a new object is created in the staging outbox folder in the S3 bucket',
         eventBus,
         enabled: true,
-        eventPattern: {
-          account: [Stack.of(this).account],
-          region: [Stack.of(this).region],
-          source: ['aws.s3'],
-          resources: [this.objectsBucket.bucketArn],
-          detailType: ['Object Created'],
-          detail: {
-            object: {
-              key: [{ prefix: STAGING_OUTBOX_PREFIX }],
-            },
-          },
-        },
+        eventPattern: this.objectCreatedEventPattern(STAGING_OUTBOX_PREFIX),
+      },
+    );
+    // - triggers when a new object is created in the outbox
+    this.outboxObjectCreatedRule = new events.Rule(
+      this,
+      'OutboxObjectCreatedRule',
+      {
+        description: 'Triggers when a new object is created in the outbox folder in the S3 bucket',
+        eventBus,
+        enabled: true,
+        eventPattern: this.objectCreatedEventPattern(OUTBOX_PREFIX),
       },
     );
   }
@@ -161,6 +159,16 @@ export class ObjectStore extends Construct {
     return this.objectsBucket.grantPut(grantee, OUTBOX_PREFIX + '*');
   }
 
+  /** Grants a given principal "Get" access from the outbox. */
+  grantGetFromOutbox(grantee: iam.IGrantable): iam.Grant {
+    return this.objectsBucket.grantRead(grantee, OUTBOX_PREFIX + '*');
+  }
+
+  /** Grants a given principal "Put" access to the staging outbox. */
+  grantPutIntoStagingOutbox(grantee: iam.IGrantable): iam.Grant {
+    return this.objectsBucket.grantPut(grantee, STAGING_OUTBOX_PREFIX + '*');
+  }
+
   /** Grants a given principal "Get" access from the staging outbox. */
   grantGetFromStagingOutbox(grantee: iam.IGrantable): iam.Grant {
     return this.objectsBucket.grantRead(grantee, STAGING_OUTBOX_PREFIX + '*');
@@ -169,5 +177,22 @@ export class ObjectStore extends Construct {
   /** Grants a given principal "Put" access to the objects folder. */
   grantPutIntoObjectsFolder(grantee: iam.IGrantable): iam.Grant {
     return this.objectsBucket.grantPut(grantee, OBJECTS_FOLDER_PREFIX + '*');
+  }
+
+  // creates an `EventPattern` that triggers when an object is created in
+  // a given folder in the objects bucket.
+  private objectCreatedEventPattern(pathPrefix: string): events.EventPattern {
+    return {
+      account: [Stack.of(this).account],
+      region: [Stack.of(this).region],
+      source: ['aws.s3'],
+      resources: [this.objectsBucket.bucketArn],
+      detailType: ['Object Created'],
+      detail: {
+        object: {
+          key: [{ prefix: pathPrefix }],
+        },
+      },
+    };
   }
 }
