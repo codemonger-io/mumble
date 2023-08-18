7c77849cbb7dfb929ecf3f25b8ad4bf37b3e2f05
Kikuo Emoto <kemoto@codemonger.io>
2023-08-18 16:08:18 +0900

    feat(viewer): add basepath config
    - The viewer app introduces a new environment variable
      `DISTRIBUTION_BASEPATH` that specifies the basepath where the actual
      service is to be served. A single slash ('/') by default.
    
    issue codemonger-io/mumble#12

--- cdk/viewer/vite.config.ts
+++ cdk/viewer/vite.config.ts
@@ -4,7 +4,14 @@ import { qwikCity } from "@builder.io/qwik-city/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig(() => {
+  let base = process.env.DISTRIBUTION_BASEPATH ?? '/';
+  if (!base.endsWith('/')) {
+    console.warn(`DISTRIBUTION_BASEPATH '${base}' should end with '/'`);
+    base += '/';
+  }
+
   return {
+    base,
     plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
     preview: {
       headers: {
