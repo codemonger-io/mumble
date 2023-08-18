e6890929a8fe22b70f1999833b2ed9d880616638
Kikuo Emoto <kemoto@codemonger.io>
2023-08-19 00:43:41 +0900

    feat(viewer): get domain name from SSM
    - Obtains the domain name from Parameter Store on AWS Systems Manager.
      The path (name) of the parameter has to be specified to
      `DOMAIN_NAME_PARAMETER_PATH` environment variable.
    
    issue codemonger-io/mumble#12

--- cdk/viewer/src/entry_aws-lambda.tsx
+++ cdk/viewer/src/entry_aws-lambda.tsx
@@ -15,11 +15,32 @@ import {
 import qwikCityPlan from "@qwik-city-plan";
 import { manifest } from "@qwik-client-manifest";
 import serverless from "serverless-http";
+import { GetParameterCommand, SSMClient } from "@aws-sdk/client-ssm";
 import render from "./entry.ssr";
 
 declare global {
   interface QwikCityPlatform extends PlatformNode {}
 }
+
+// optionally obtains the domain name from the Parameter Store on SSM
+async function getDomainNameParameter(): Promise<string | null> {
+  const parameterName = process.env.DOMAIN_NAME_PARAMETER_PATH;
+  console.log("obtaining domain name from Parameter Store", parameterName);
+  if (parameterName == null) {
+    console.log("no domain name parameter configured");
+    return null;
+  }
+  const ssm = new SSMClient({});
+  const res = await ssm.send(new GetParameterCommand({
+    Name: parameterName,
+    WithDecryption: true,
+  }));
+  // service should not start with a bad configuration
+  return res.Parameter?.Value ?? null;
+}
+
+const DOMAIN_NAME = await getDomainNameParameter();
+console.log("DOMAIN_NAME", DOMAIN_NAME);
 
 // Create the Qwik City router
 const { router, notFound, staticFile } = createQwikCity({
@@ -33,6 +54,9 @@ const { router, notFound, staticFile } = createQwikCity({
   getOrigin(req) {
     if (process.env.IS_OFFLINE) {
       return `http://${req.headers.host}`;
+    }
+    if (DOMAIN_NAME != null) {
+      return `https://${DOMAIN_NAME}`;
     }
     return null;
   },
