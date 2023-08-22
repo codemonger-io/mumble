36c0390d21833951ee062d6f3f161730e765948f
Kikuo Emoto <kemoto@codemonger.io>
2023-08-22 16:45:25 +0900

    feat(viewer): handle no user
    - The user profile page shows a 404 error page if the user is not found.
    
    issue codemonger-io/mumble#12

--- cdk/viewer/src/routes/users/[id]/layout.tsx
+++ cdk/viewer/src/routes/users/[id]/layout.tsx
@@ -4,23 +4,35 @@ import { routeLoader$ } from '@builder.io/qwik-city';
 
 import type { UserInfo } from '~/types/user-info';
 import { getDynamoDbClient } from '~/utils/dynamodb';
+import { isFailReturn } from '~/utils/fail-return';
 
 // fetches user information from the database
-export const useUserInfo = routeLoader$(async ({ params }) => {
+export const useUserInfo = routeLoader$(async requestEvent => {
+  const userId = requestEvent.params.id;
+  console.log('obtaining user info for:', userId);
   const client = getDynamoDbClient();
   const res = await client.send(new GetCommand({
     TableName: process.env.USER_TABLE_NAME,
     Key: {
-      'pk': `user:${params.id}`,
+      'pk': `user:${userId}`,
       'sk': 'reserved',
     },
   }));
+  if (res.Item == null) {
+    // no user found
+    return requestEvent.fail(404, {
+      errorMessage: 'User not found',
+    });
+  }
   // TODO: verify UserInfo
   return res.Item as UserInfo;
 });
 
 export default component$(() => {
   const userInfo = useUserInfo();
+  if (isFailReturn(userInfo.value)) {
+    return <p>{userInfo.value.errorMessage}</p>
+  }
 
   return (
     <>
