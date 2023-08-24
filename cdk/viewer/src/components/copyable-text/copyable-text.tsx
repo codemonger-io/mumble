84e4460ac85b2350ea961817a0b742a9648ca0ba
Kikuo Emoto <kemoto@codemonger.io>
2023-08-24 22:53:26 +0900

    feat(viewer): add CopyableText component
    - Introduces a new component `CopyableText` that displays a text with a
      button to copy the text to clipboard.
    - Also introduces new global CSS vars:
        - `--border-radius`
        - `--theme-color-success`
        - `--theme-color-error`
    
    issue codemonger-io/mumble#12

---
+++ cdk/viewer/src/components/copyable-text/copyable-text.tsx
@@ -0,0 +1,60 @@
+import { component$, useSignal, useTask$ } from '@builder.io/qwik';
+import { LuClipboardCopy } from '@qwikest/icons/lucide';
+
+import styles from './copyable-text.module.css';
+
+interface CopyableTextProps {
+  text: string;
+}
+
+export default component$(({ text }: CopyableTextProps) => {
+  const isHovered = useSignal(false);
+  const copyResult = useSignal(null as null | 'success' | 'error');
+  const copyResultMessage = useSignal('');
+
+  useTask$(async ({ track, cleanup }) => {
+    track(() => copyResult.value);
+    if (copyResult.value == null) {
+      return;
+    }
+    const timeoutId = setTimeout(() => {
+      copyResult.value = null;
+    }, 1000);
+    cleanup(() => {
+      clearTimeout(timeoutId);
+      copyResult.value = null;
+    });
+  });
+
+  return (
+    <span
+      class={[styles.container, { [styles.hovered]: isHovered.value }]}
+      onPointerEnter$={() => isHovered.value = true}
+      onPointerLeave$={() => isHovered.value = false}
+    >
+      <span class={styles.clipped}>
+        <span class={styles.text}>{text}</span>
+      </span>
+      <button
+        class={styles['copy-button']}
+        onClick$={async () => {
+          try {
+            await navigator.clipboard.writeText(text);
+            copyResultMessage.value = 'Copied to clipboard';
+            copyResult.value = 'success';
+          } catch (err) {
+            console.error(err);
+            copyResultMessage.value = 'Clipboard unavailable';
+            copyResult.value = 'error';
+          }
+        }}
+      ><LuClipboardCopy /></button>
+      <span
+        class={[
+          styles['copy-notice'],
+          copyResult.value && styles[copyResult.value],
+        ]}
+      >{copyResultMessage.value}</span>
+    </span>
+  );
+});
