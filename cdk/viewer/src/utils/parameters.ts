225ddde241f10b81c6348ac43f617b0860bf0f31
Kikuo Emoto <kemoto@codemonger.io>
2023-10-11 21:48:23 +0900

    feat(viewer): add OpenAI API key param
    - `utils/parameters.ts` introduces a new function
      `getOpenAiApiKeyParameter` that obtains the OpenAI API key from the
      Parameter Store on AWS Systems Manager. It reads the parameter path
      from the environment variable `OPENAI_API_KEY_PARAMETER_PATH`.
      `getDomainNameParameter` and `getOpenAiApiKeyParameter` shares a new
      function `getParameter` that performs actual requests to AWS.
    
    issue codemonger-io/mumble#26

--- cdk/viewer/src/utils/parameters.ts
+++ cdk/viewer/src/utils/parameters.ts
@@ -1,5 +1,8 @@
 import { GetParameterCommand, SSMClient } from "@aws-sdk/client-ssm";
 
+// AWS Systems Manager client.
+let ssm: SSMClient | null = null;
+
 /**
  * Obtains the optional domain name from the Parameter Store on AWS Systems
  * Manager.
@@ -11,11 +14,42 @@ export async function getDomainNameParameter(): Promise<string | null> {
     console.log("no domain name parameter configured");
     return null;
   }
-  const ssm = new SSMClient({});
+  return await getParameter(parameterName);
+}
+
+/**
+ * Obtains the OpenAI API key from the Parameter Store on AWS Systems Manager.
+ */
+export async function getOpenAiApiKeyParameter(): Promise<string> {
+  const parameterName = process.env.OPENAI_API_KEY_PARAMETER_PATH;
+  console.log("obtaining OpenAI API key from Parameter Store", parameterName);
+  if (parameterName == null) {
+    throw new Error("no parameter path to OpenAI API key configured");
+  }
+  const apiKey = await getParameter(parameterName);
+  if (apiKey == null) {
+    throw new Error("no OpenAI API key configured");
+  }
+  return apiKey;
+}
+
+/**
+ * Obtains a parameter from the Parameter Store on AWS Systems Manager.
+ *
+ * @returns
+ *
+ *   `null` if no parameter is configured in the Parameter Store on AWS Systems
+ *   Manager.
+ */
+export async function getParameter(
+  parameterName: string,
+): Promise<string | null> {
+  if (ssm == null) {
+    ssm = new SSMClient({});
+  }
   const res = await ssm.send(new GetParameterCommand({
     Name: parameterName,
     WithDecryption: true,
   }));
-  // service should not start with a bad configuration
   return res.Parameter?.Value ?? null;
 }
