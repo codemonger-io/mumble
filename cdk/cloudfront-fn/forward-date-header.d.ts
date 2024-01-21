1e24d1dde482901fb3984218172e000c44a91e60
Kikuo Emoto <kemoto@codemonger.io>
2024-01-21 11:15:11 +0900

    chore(cdk): add type-check script
    - `cdk` introduces `type-check` script that runs TypeScript for type
      checking.
    - Fixes `cloudfront-fn/*.d.ts` where function type definitions were
      incorrect.
    - `tsconfig.json` excludes `lambda` folder.

--- cdk/cloudfront-fn/forward-date-header.d.ts
+++ cdk/cloudfront-fn/forward-date-header.d.ts
@@ -1,7 +1,7 @@
 // this declaration exists only for unit tests.
 import type { Event, Request } from './types';
 
-function forwardDateHeader(event: Event): Request;
+type ForwardDateHeader = (event: Event) => Request;
 
 // declaration of the __get__ function injected by babel-plugin-rewire.
-export function __get__(name: 'forwardDateHeader'): forwardDateHeader;
+export function __get__(name: 'forwardDateHeader'): ForwardDateHeader;
--- cdk/cloudfront-fn/forward-host-header.d.ts
+++ cdk/cloudfront-fn/forward-host-header.d.ts
@@ -1,7 +1,7 @@
 // this declaration exists only for unit tests.
 import type { Event, Request } from './types';
 
-function forwardHostHeader(event: Event): Request;
+type ForwardHostHeader = (event: Event) => Request;
 
 // declaration of the __get__ function injected by babel-plugin-rewire.
-export function __get__(name: 'forwardHostHeader'): forwardHostHeader;
+export function __get__(name: 'forwardHostHeader'): ForwardHostHeader;
